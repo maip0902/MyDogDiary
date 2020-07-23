d3735658d42f716432b527cbdf4ac858410fd2b4
神田舞 <maiptomato@gmail.com>
2020-07-24 00:13:42 +0900

    Firebaseでユーザー登録まで

--- app/src/main/java/com/example/mydogdiary/MainActivity.kt
+++ app/src/main/java/com/example/mydogdiary/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mydogdiary
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
