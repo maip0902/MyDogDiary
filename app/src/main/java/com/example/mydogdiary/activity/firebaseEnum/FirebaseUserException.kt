206bbc7e0c738f4f585a97eb092451e9febd6a61
神田舞 <maiptomato@gmail.com>
2020-08-01 13:38:09 +0900

    ログイン処理

--- app/src/main/java/com/example/mydogdiary/activity/firebaseEnum/FirebaseUserException.kt
+++ app/src/main/java/com/example/mydogdiary/activity/firebaseEnum/FirebaseUserException.kt
@@ -1,20 +1,30 @@
 package com.example.mydogdiary.activity.firebaseEnum
 
 enum class FirebaseUserException(val exception: String) {
-    invalidEmail("The email address is already in use by another account") {
+    alreadyUsedEmail("The email address is already in use by another account") {
         override fun returnErrorMessage(): String {
             return "すでに登録されているメールアドレスです"
         }
     },
-    shortPassword("The email address is badly formatted."){
+    invalidEmail("The email address is badly formatted."){
         override fun returnErrorMessage(): String {
             return "正しいメールアドレスの形式で入力してください"
 
         }
     },
-    alreadyUsedEmail("The given password is invalid. [ Password should be at least 6 characters ]"){
+    shortPassword("The given password is invalid. [ Password should be at least 6 characters ]"){
         override fun returnErrorMessage(): String {
             return "パスワードは6文字以上で設定してください"
+        }
+    },
+    cannotLogin("The password is invalid or the user does not have a password.") {
+        override fun returnErrorMessage(): String {
+            return "メールアドレスもしくはパスワードが間違っています"
+        }
+    },
+    otherwise("") {
+        override fun returnErrorMessage(): String {
+            return "何らかのエラーが発生しました"
         }
     };
 
