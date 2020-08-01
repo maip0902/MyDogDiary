1a299285369793364379efe2faa76835a46b1bc6
神田舞 <maiptomato@gmail.com>
2020-08-01 12:31:23 +0900

    ログイン処理の画面の作成

--- app/src/main/java/com/example/mydogdiary/activity/activity/MainActivity.kt
+++ app/src/main/java/com/example/mydogdiary/activity/activity/MainActivity.kt
@@ -16,5 +16,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
         }
+
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
